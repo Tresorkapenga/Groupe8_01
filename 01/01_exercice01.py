f54b9a67a150b410b4db6502592d2b90791706e0
Tresorkapenga <tresoredimba10@gmail.com>
2025-07-15 13:03:37 +0000

    Ajouter le fichier sur github

---
+++ 01/01_exercice01.py
@@ -0,0 +1,14 @@
+# Demande d'informations à L'utilisateur
+prenom = input ("Entrez votre prénom : ")
+age = int (input ( "Entrez votre âge "))
+ville = input("Entrez votre ville ")
+metier = input ("Entrez votre métier : " )
+# Approximation des jours vécus
+jours_vécus = age* 365
+# Affichage formaté
+print("\n=== PROFIL UTILISATEUR ===" )
+print(f"Prénom: {prenom}")
+print(f"Âge : {age} ans ({jours_vécus} jours vécus environ)")
+print(f"Ville : {ville}")
+print(f"Métier: {metier}")
+
